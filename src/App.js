906bb44050ce6815db101fcd2afbaea3b6a1998c
FitzroyGarcia <herberthfitzroy@gmail.com>
2024-04-12 01:20:05 -0500

    Instalancion de PrimeReact

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+
+import 'primereact/resources/themes/saga-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+
+import { PrimeReactProvider } from 'primereact/api';
+
+import MainTable from "./components/main/MainTable"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PrimeReactProvider>
+      <MainTable></MainTable>
+    </PrimeReactProvider>
   );
 }
 
